ce66c6166ad1ccb7da3280f89dfbfa86e725e7d3
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-15 21:41:39 +0200

    minor tweak

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Menu')
 @section('content')
 
-    <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32 h-screen">
         <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
             aria-hidden="true">
             <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
