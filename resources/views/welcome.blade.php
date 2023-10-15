7cfdc2455e0c9d07abe078638d16baf338231658
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-15 18:58:39 +0200

    first draft

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,55 +16,39 @@
 
 </head>
 
-<body class="antialiased">
-    <div class="relative min-h-screen bg-center dark:bg-gray-950 selection:text-white">
+<body class="antialiased relative">
+    <div class="min-h-screen bg-[url('/public/img/Ristorante_ai_Murazzi.png')] bg-contain selection:text-white">
+        <!-- Background Overlay -->
+        <div class="absolute inset-0 bg-black opacity-50 z-0"></div>
+
+        <!-- Navigation -->
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-1 lg:px-8" aria-label="Global">
             @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/dashboard') }}"
-                            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}"
-                            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500">Log
-                            in</a>
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                @auth
+                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500">Dashboard</a>
+                @else
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}"
-                                class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500">Register</a>
-                        @endif
-                    @endauth
-        </nav>
-        @endif
-
-        <div class=" ">
-            <div class="flex justify-center">
-
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500">Register</a>
+                @endif
+                @endauth
             </div>
+            @endif
+        </nav>
 
-            <div class="mt-16">
-                <div class=" w-full relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-                    <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
-                        aria-hidden="true">
-                        <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-                            style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
-                        </div>
-                    </div>
-                    <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
-                        aria-hidden="true">
-                        <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-                            style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
-                        </div>
-                    </div>
-                    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                        <div class="mx-auto max-w-2xl lg:mx-0">
-                            <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Benvenuto Nel pannello
-                                di controllo</h2>
-                            <p class="mt-6 text-lg leading-8 text-gray-300">Esegui il login ed accedi.</p>
-                        </div>
+        <!-- Content -->
+        <div class="relative z-10">
+            <div class="flex justify-center w-full py-32 sm:py-48 lg:py-56">
+                <!-- Your title and content -->
+                <div class="text-center">
+                    <div class="lg:text-9xl font-edmund text-white sm:text-6xl">Ristorante ai Murazzi</div>
+                    <p class="mt-6 text-3xl leading-8 text-white">Effettua il login ed accedi alla tua Dashboard.</p>
+                    <div class="mt-10 flex items-center justify-center gap-x-6">
+                        <a href="#" class="rounded-full bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Accedi</a>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
