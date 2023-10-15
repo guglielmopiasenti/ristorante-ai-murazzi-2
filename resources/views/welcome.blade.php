ec6c771073048b2454b2299d87078387c62d1b1a
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-15 19:12:41 +0200

    added button to access and fixed route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
                     <div class="lg:text-9xl font-edmund text-white sm:text-6xl">Ristorante ai Murazzi</div>
                     <p class="mt-6 text-3xl leading-8 text-white">Effettua il login ed accedi alla tua Dashboard.</p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="#" class="rounded-full bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Accedi</a>
+                        <a href="{{ route('login') }}" class="rounded-full bg-indigo-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Accedi</a>
                     </div>
                 </div>
             </div>
