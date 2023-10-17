0d01a877f2e9911595065a9409a56e7c0d2c491d
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-17 12:39:05 +0200

    form first draft

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -3,16 +3,24 @@
 @section('content')
     <div>
         @if ($dish->exists)
-            <div class="container-fluid px-0 overflow-x-hidden">
-                <video autoplay muted preload="auto" class="object-fit-contain">
-                    <source src="{{ asset('edit-dish.mp4') }}" type="video/mp4">
-                </video>
+            <div class="bg-gray-900 lg:py-20 sm:py-20">
+                <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div class="mx-auto max-w-2xl lg:text-center">
+                        <h2 class="text-base font-semibold leading-7 text-indigo-600">Modifica velocemente un piatto</h2>
+                        <p class="my-6 lg:text-6xl font-bold tracking-tight text-white sm:text-4xl">Modifica un Piatto</p>
+                        <p class="mt-6 text-lg leading-8 text-gray-600">Completa il form e modifica un piatto esistente.</p>
+                    </div>
+                </div>
             </div>
         @else
-            <div class="container-fluid px-0 overflow-x-hidden">
-                <video autoplay muted preload="auto" class="object-fit-contain">
-                    <source src="{{ asset('create-dish.mp4') }}" type="video/mp4">
-                </video>
+            <div class="bg-gray-900 lg:py-20 sm:py-20">
+                <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div class="mx-auto max-w-2xl lg:text-center">
+                        <h2 class="text-base font-semibold leading-7 text-indigo-600">Aggiungi velocemente un piatto</h2>
+                        <p class="my-6 lg:text-6xl font-bold tracking-tight text-white sm:text-4xl">Crea un Piatto</p>
+                        <p class="mt-6 text-lg leading-8 text-gray-600">Completa il form e aggiungi un piatto al menu.</p>
+                    </div>
+                </div>
             </div>
         @endif
     </div>
@@ -20,7 +28,6 @@
     <div class="content">
         <div class="container mx-auto">
             <div class="p-5">
-
                 <form class="my-5" method="POST" enctype="multipart/form-data" novalidate
                     @if ($dish->exists) action="{{ route('admin.dishes.update', $dish->id) }}"
                     @else action="{{ route('admin.dishes.store') }}" @endif
@@ -32,46 +39,56 @@
 
                     <!-- Dish Name -->
                     <div class="mb-3">
-                        <label for="dishName" class="block text-sm font-medium leading-6 text-gray-400">Dish Name <sup class="text-danger">*</sup></label>
-                        <input name="name" type="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('name') is-invalid @enderror form-control"
+                        <label for="dishName" class="block text-sm font-medium leading-6 text-gray-400">Nome Piatto <sup
+                                class="text-danger">*</sup></label>
+                        <input name="name" type="text"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @if (!$errors->has('name')) border-green-300 @endif"
                             id="dishName" value="{{ old('name', $dish->name) }}" required>
-                        <small id="dish-name-error" class="text-danger"></small>
+                        <small id="dish-name-error" class="text-red-500"></small>
                         @error('name')
-                            <div class="invalid-feedback">
+                            <div class="text-red-500 text-sm mt-2">
                                 {{ $message }}
                             </div>
                         @enderror
                     </div>
 
                     <!-- Dish Image -->
-                    <div class="mb-3">
-                        <label for="dishImage" class="block text-sm font-medium leading-6 text-gray-400">Dish Image</label>
-                        <input name="image" type="file" class="@error('image') is-invalid @enderror form-control"
-                            id="dishImage">
-                        <input type="hidden" name="current_image" value="{{ $dish->image }}">
-                        @error('image')
-                            <div class="invalid-feedback">
-                                {{ $message }}
+                    <div x-data="{ imagePreview: '{{ $dish->image ? asset('storage/' . $dish->image) : asset('storage/placeholder.jpg') }}' }" class="col-span-full">
+                        <label for="dishImage" class="block text-sm font-medium leading-6 text-gray-400">Immagine
+                            Piatto</label>
+                        <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-400 px-6 py-10">
+                            <div class="text-center">
+                                <img x-bind:src="imagePreview" class="mx-auto h-32 w-32 object-cover">
+                                <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                                    <label for="file-upload"
+                                        class="relative cursor-pointer rounded-md bg-white px-2 font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                        <span>Carica</span>
+                                        <input x-on:change="imagePreview = URL.createObjectURL($event.target.files[0])"
+                                            name="image" type="file" class="sr-only" id="file-upload">
+                                    </label>
+                                    <p class="pl-1">o droppa img</p>
+                                </div>
+                                <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF</p>
                             </div>
-                        @enderror
+                        </div>
                     </div>
-
-                    <div class="mb-3">
-                        @if (!$dish->image)
-                            <img src="{{ asset('storage/placeholder.jpg') }}" class="object-cover h-85 w-85" id="preview">
-                        @elseif($dish->image)
-                            <img src="{{ asset('storage/' . $dish->image) }}" class="object-cover h-85 w-85" id="preview">
-                        @endif
-                    </div>
+                    <input type="hidden" name="current_image" value="{{ $dish->image }}">
+                    @error('image')
+                        <div class="text-red-500 text-sm mt-2">
+                            {{ $message }}
+                        </div>
+                    @enderror
 
                     <!-- Dish Price -->
-                    <div class="mb-3">
-                        <label for="dishPrice" class="block text-sm font-medium leading-6 text-gray-400">Dish Price <sup class="text-danger">*</sup></label>
-                        <input name="price" type="number" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('price') is-invalid @enderror form-control"
+                    <div class="my-3">
+                        <label for="dishPrice" class="block text-sm font-medium leading-6 text-gray-400">Dish Price <sup
+                                class="text-danger">*</sup></label>
+                        <input name="price" type="number"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @if (!$errors->has('price')) border-green-300 @endif"
                             id="dishPrice" value="{{ old('price', $dish->price) }}" required>
                         <small id="dish-price-error" class="text-danger"></small>
                         @error('price')
-                            <div class="invalid-feedback">
+                            <div class="text-red-500 text-sm mt-2">
                                 {{ $message }}
                             </div>
                         @enderror
@@ -79,12 +96,14 @@
 
                     <!-- Dish Ingredients -->
                     <div class="mb-3">
-                        <label for="dishIngredients" class="block text-sm font-medium leading-6 text-gray-400">Dish Ingredients <sup class="text-danger">*</sup></label>
-                        <textarea name="ingredients" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('ingredients') is-invalid @enderror form-control" id="dishIngredients"
-                            required>{{ old('ingredients', $dish->ingredients) }}</textarea>
+                        <label for="dishIngredients" class="block text-sm font-medium leading-6 text-gray-400">Dish
+                            Ingredients <sup class="text-danger">*</sup></label>
+                        <textarea name="ingredients"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @if (!$errors->has('ingredients')) border-green-300 @endif"
+                            id="dishIngredients" required>{{ old('ingredients', $dish->ingredients) }}</textarea>
                         <small id="dish-ingredients-error" class="text-danger"></small>
                         @error('ingredients')
-                            <div class="invalid-feedback">
+                            <div class="text-red-500 text-sm mt-2">
                                 {{ $message }}
                             </div>
                         @enderror
@@ -92,11 +111,13 @@
 
                     <!-- Dish Description -->
                     <div class="mb-3">
-                        <label for="dishDescription" class="block text-sm font-medium leading-6 text-gray-400">Dish Description</label>
-                        <textarea name="description" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('description') is-invalid @enderror form-control"
+                        <label for "dishDescription" class="block text-sm font-medium leading-6 text-gray-400">Dish
+                            Description</label>
+                        <textarea name="description"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @if (!$errors->has('description')) border-green-300 @endif"
                             id="dishDescription">{{ old('description', $dish->description) }}</textarea>
                         @error('description')
-                            <div class="invalid-feedback">
+                            <div class="text-red-500 text-sm mt-2">
                                 {{ $message }}
                             </div>
                         @enderror
@@ -105,39 +126,19 @@
                     <!-- Dish visibility -->
                     <div class="mb-3">
                         <label for="is_visible" class="block text-sm font-medium leading-6 text-gray-400">Visible?</label>
-                        <input value="1" type="checkbox" name="is_visible" id="is_visible"
-                            {{ old('is_visible', $dish->is_visible) ? 'checked' : '' }}>
+                        <input value="1" type="checkbox"
+                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" name="is_visible"
+                            id="is_visible" {{ old('is_visible', $dish->is_visible) ? 'checked' : '' }}>
                     </div>
 
                     <!-- Submit Button -->
                     <div class="mt-6 flex items-center justify-end gap-x-6">
                         <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                        <button type="submit"
+                            class="rounded-full bg-indigo-600 px-3 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-    
 @endsection
-
-@section('scripts')
-    @vite('resources/js/image-preview.js');
-    <script>
-        document.getElementById('dishImage').addEventListener('change', function(e) {
-            var preview = document.getElementById('preview');
-            var fileInput = e.target;
-
-            if (fileInput.files && fileInput.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function(e) {
-                    preview.src = e.target.result;
-                }
-
-                reader.readAsDataURL(fileInput.files[0]);
-            }
-        });
-    </script>
-@endsection
-
