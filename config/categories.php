53832ae6439711c3a341fb5cff3c9e352837d810
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-21 00:10:58 +0200

    added category file

---
+++ config/categories.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'categories' => [
+    ['name' => 'Antipasti'],
+    ['name' => 'Primi'],
+    ['name' => 'Secondi'],
+    ['name' => 'Dolci'],
+    ['name' => 'Bevande'],
+    ['name' => 'Vini bianchi'],
+    ['name' => 'Vini rossi'],
+    ['name' => 'Birre'],
+    ['name' => 'Drink'],
+    ['name' => 'Caffetteria'],
+  ],
+];
