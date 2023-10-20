797c69305f5e0fc7b4dea6754a059ebe9b713000
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-20 17:02:29 +0200

    finished index pictures

--- resources/views/admin/pictures/index.blade.php
+++ resources/views/admin/pictures/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', 'Menu')
 @section('content')
-    <div x-data="{ openDeleteModal: false, pictureId: null }">
+    <div x-data="{ openDeleteModal: false, openCreatePlate: false, pictureId: null }">
         <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
             <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
                 aria-hidden="true">
@@ -25,17 +25,28 @@
         </div>
         <div class="bg-grey-900">
             <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                <h2 class="text-4xl font-bold tracking-tight text-white pb-6">Foto attualmente online</h2>
+                <div class="grid grid-cols-2 items-center">
+                    <h2 class="text-4xl font-bold tracking-tight text-white pb-6">Foto attualmente online</h2>
+                    <div class="w-auto justify-self-end">
+                        <button @click="openCreatePlate = true" x-transition type="button"
+                            class="rounded-full border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Aggiungi una foto
+                        </button>
+                        <template x-if="openCreatePlate" x-cloak>
+                            @include('includes.add-picture')
+                        </template>
+                    </div>
+                </div>
 
                 <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     @forelse ($pictures as $picture)
-                        <div class="group relative">
+                        <div class="group relative grid grid-cols-1">
                             <div
                                 class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                 <img src="{{ asset('storage/' . $picture->filename) }}" alt="Ristorante ai Murazzi."
                                     class="h-full w-full object-cover object-center lg:h-full lg:w-full">
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="mt-4 justify-self-center">
                                 <button @click="openDeleteModal = true, pictureId = {{ $picture->id }}" x-transition
                                     class="rounded-full bg-red-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-red-500">
                                     Elimina
