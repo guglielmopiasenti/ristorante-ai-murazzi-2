d12d62d8ad19645d0534b984571b88c8a7591ac4
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-20 14:54:16 +0200

    setting database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([UserSeeder::class, DishSeeder::class]);
+        $this->call([UserSeeder::class, DishSeeder::class, PictureSeeder::class]);
     }
 }
