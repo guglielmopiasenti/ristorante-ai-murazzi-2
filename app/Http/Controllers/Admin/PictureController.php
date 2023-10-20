2c0690b9c5b53766a3198713563ae1ffcd4f7864
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-20 14:53:13 +0200

    creating and setting picture controller

---
+++ app/Http/Controllers/Admin/PictureController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Picture;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+
+class PictureController extends Controller
+{
+    public function index()
+    {
+        $pictures = Picture::all();
+        return view('admin.pictures.index', compact('pictures'));
+    }
+
+    public function create()
+    {
+        return view('admin.pictures.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'filename' => 'required|image',
+        ]);
+
+        $data = $request->all();
+
+        if (array_key_exists('filename', $data)) {
+            $image_url = Storage::disk('public')->put('pictures', $data['filename']);
+            $data['filename'] = $image_url;
+        } else {
+            $data['image'] = 'placeholder.jpg';
+        }
+
+        $picture = new Picture();
+        $picture->fill($data);
+        $picture->save();
+
+        return redirect()->route('admin.pictures.index');
+    }
+
+    public function drop(string $id)
+    {
+        $picture = Picture::findOrFail($id);
+        $picture->forceDelete();
+        return redirect()->route('admin.pictures.index');
+    }
+}
