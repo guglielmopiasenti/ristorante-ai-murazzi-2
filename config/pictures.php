92cf723b72ecc77a9b96cd7612b160ffc5f27d83
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-20 14:53:45 +0200

    setting pictures array

---
+++ config/pictures.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+  [
+    "filename" => "pictures/murazzi_1.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_2.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_3.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_4.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_5.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_6.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_7.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_8.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_9.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_10.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_11.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_12.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_13.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_14.avif",
+  ],
+  [
+    "filename" => "pictures/murazzi_15.jpeg",
+  ],
+];
