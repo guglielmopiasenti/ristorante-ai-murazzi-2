0d3cbe9ec3506d3b71986b32ec57c83e7f5c1846
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-21 11:45:26 +0200

    fixed form style

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -77,42 +77,55 @@
                         @enderror
                     </div>
 
-                    <!-- Dish Price -->
-                    <div class="my-3">
-                        <label for="dishPrice" class="block text-sm font-medium leading-6 text-gray-400">Dish Price <sup
-                                class="text-red-500">*</sup></label>
-                        <input name="price" type="number"
-                            class="block w-full rounded-lg py-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('price') border-red-500 @enderror"
-                            id="dishPrice" value="{{ old('price', $dish->price) }}" required>
-                        @error('price')
-                            <div class="text-red-500 text-sm mt-2">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
+                    <div class="grid grid-cols-3 gap-20 items-center">
 
-                    {{-- Dish Categories --}}
-                    <div class="my-3">
-                        <label for="category" class="block text-sm font-medium leading-6 text-gray-400">Categoria <sup
-                                class="text-red-500">*</sup></label>
-                        <select id="category" name="category" autocomplete="category-type"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                            @foreach ($categories as $category)
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endforeach
-                        </select>
+                        <!-- Dish Price -->
+                        <div class="my-3">
+                            <label for="dishPrice" class="block text-sm font-medium leading-6 text-gray-400">Prezzo <sup
+                                    class="text-red-500">*</sup></label>
+                            <input name="price" type="number"
+                                class="block w-full rounded-lg py-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('price') border-red-500 @enderror"
+                                id="dishPrice" value="{{ old('price', $dish->price) }}" required>
+                            @error('price')
+                                <div class="text-red-500 text-sm mt-2">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
 
-                        @error('category')
-                            <div class="text-red-500 text-sm mt-2">
-                                {{ $message }}
-                            </div>
-                        @enderror
+                        {{-- Dish Categories --}}
+                        <div class="my-3">
+                            <label for="category" class="block text-sm font-medium leading-6 text-gray-400">Categoria <sup
+                                    class="text-red-500">*</sup></label>
+                            <select id="category" name="category" autocomplete="category-type"
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+
+                            @error('category')
+                                <div class="text-red-500 text-sm mt-2">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <!-- Dish visibility -->
+                        <div class="flex items-end gap-2 mt-5">
+                            <input value="1" type="checkbox"
+                                class="h-6 w-6 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                name="is_visible" id="is_visible"
+                                {{ old('is_visible', $dish->is_visible) ? 'checked' : '' }}>
+                            <label for="is_visible" class="block text-sm font-medium leading-6 text-gray-400">Visibile
+                                online?</label>
+                        </div>
                     </div>
 
                     <!-- Dish Ingredients -->
                     <div class="mb-3">
-                        <label for="dishIngredients" class="block text-sm font-medium leading-6 text-gray-400">Dish
-                            Ingredients <sup class="text-red-500">*</sup></label>
+                        <label for="dishIngredients" class="block text-sm font-medium leading-6 text-gray-400">Ingredienti
+                            <sup class="text-red-500">*</sup></label>
                         <textarea name="ingredients"
                             class="block w-full rounded-lg border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('ingredients') border-red-500 @enderror"
                             id="dishIngredients" required>{{ old('ingredients', $dish->ingredients) }}</textarea>
@@ -125,8 +138,8 @@
 
                     <!-- Dish Description -->
                     <div class="mb-3">
-                        <label for "dishDescription" class="block text-sm font-medium leading-6 text-gray-400">Dish
-                            Description</label>
+                        <label for "dishDescription" class="block text-sm font-medium leading-6 text-gray-400">
+                            Descrizione</label>
                         <textarea name="description"
                             class="block w-full rounded-lg border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('description') border-red-500 @enderror"
                             id="dishDescription">{{ old('description', $dish->description) }}</textarea>
@@ -137,19 +150,11 @@
                         @enderror
                     </div>
 
-                    <!-- Dish visibility -->
-                    <div class="mb-3">
-                        <label for="is_visible" class="block text-sm font-medium leading-6 text-gray-400">Visible?</label>
-                        <input value="1" type="checkbox"
-                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" name="is_visible"
-                            id="is_visible" {{ old('is_visible', $dish->is_visible) ? 'checked' : '' }}>
-                    </div>
-
                     <!-- Submit Button -->
                     <div class="mt-6 flex items-center justify-end gap-x-6">
                         <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
                         <button type="submit"
-                            class="rounded-full bg-indigo-600 px-3 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                            class="rounded-full bg-indigo-600 px-3 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Aggiungi</button>
                     </div>
                 </form>
             </div>
